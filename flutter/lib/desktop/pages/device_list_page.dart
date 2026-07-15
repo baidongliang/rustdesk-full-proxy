31c9bd13297a7ae45ef4b4d15033fc3eb2290f3e
baidongliang <421105781@qq.com>
2026-07-15 11:01:57 +0800

    chore: 更新设备列表 mock ID（重装后新 ID 1781366437）

--- flutter/lib/desktop/pages/device_list_page.dart
+++ flutter/lib/desktop/pages/device_list_page.dart
@@ -39,7 +39,7 @@ class _DeviceListPageState extends State<DeviceListPage> {
     DemoDevice(
       name: 'DWD68 定制设备',
       sn: '309ea42475e26594',
-      id: '1077664162',
+      id: '1781366437',
       password: 'xinzx2026',
     ),
   ];
